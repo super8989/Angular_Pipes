16195d6b4206aaf567346b16ba71fcee87f5885b
Sam <super8989@gmail.com>
2021-01-13 11:31:14 -0500

    Creating a Custom Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   servers = [
     {
       instanceType: 'medium',
-      name: 'Production Server',
+      name: 'Production',
       status: 'stable',
       started: new Date(15, 1, 2017),
     },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShortenPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
